9925c81ad79de726e715e4ab166f432d63570a54
azzamaflah <azzamhebat46@gmail.com>
2023-06-19 11:21:56 +0700

    mengunakan try untuk memanggil array elemen ke 5

---
+++ exeptionStandar/exeptionStandar.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+//untuk objek array yang akan kita gunakan
+using namespace std;
+
+int main() {
+	cout << "Awal Program" << endl; //penanda 1:...
+	try {
+
+	}
+}
