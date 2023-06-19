57e889e21ab7edf8bb9a05ac628d0c13baf2f54e
azzamaflah <azzamhebat46@gmail.com>
2023-06-19 11:16:18 +0700

    membuat try

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -1,20 +1,44 @@
-// ioFileWithPath.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	string baris;
+	string NamaFile;
+
+	cout << "Masukan Nama file : ";
+	cin >> NamaFile;
+
+	//membuka file dalam mode menulis
+	ofstream outfile;
+	//menunjukan ke sebuah nama file
+	outfile.open(NamaFile, ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar " << endl;
+
+	//membuka file dalam mode membaca
+	ifstream infile;
+	//menunjukan ke sebuah file
+	infile.open(NamaFile, ios::in);
+
+	cout << endl << ">= membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			// dan tampilkan di sini
+			cout << baris << '\n';
+
+		}
+
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+
+	//jika tidak ditemukan file makna akan menampilkan ini
+	else cout << " inable to open file";
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ manualExecption/manualExecption.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	try {
+		cout << "Selamat Beljar di Prodi TI UMY" << endl;
+		throw 1; //melemparkan sebuah integer maka
+	}
+}
