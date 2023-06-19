e4941532fc32c936c43f4982ac69535508159f72
azzamaflah <azzamhebat46@gmail.com>
2023-06-19 11:19:08 +0700

    membuat catch

--- manualExecption/manualExecption.cpp
+++ manualExecption/manualExecption.cpp
@@ -6,5 +6,17 @@ int main()
 	try {
 		cout << "Selamat Beljar di Prodi TI UMY" << endl;
 		throw 1; //melemparkan sebuah integer maka
+		cout << "Pernyataan tidak akan dieksekusi" << endl;
+
 	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+
+	}
+	catch (...) {
+		// jika selain integer maka blok ini akan diesekusi
+		cout << "default pengecualian dieksekusi" << endl;
+	}
+	return 0;
 }
