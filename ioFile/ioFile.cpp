bdd4c7fbca41ba93dc64c5c0d4f713e33da92640
azzamaflah <azzamhebat46@gmail.com>
2023-06-19 10:30:54 +0700

    membuat output file

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -5,4 +5,8 @@ using namespace std;
 int main() {
 	string baris;
 	
+	// membuka file dalam mode menulis.
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open("E:\pemdas\contohfile.txt");
 }
